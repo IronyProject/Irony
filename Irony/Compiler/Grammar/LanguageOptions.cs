eb8d453c6d50906a8c5cf7480bc6eb4793a2b2b6
rivantsov <rivantsov@localhost>
2008-09-05 23:53:21 +0000

    minor code refactoring

---
+++ Irony/Compiler/Grammar/LanguageOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Irony.Compiler {
+
+  [Flags]
+  public enum LanguageOptions {
+    None = 0,
+    TailRecursive = 0x01,
+    AutoNewLine = 0x02,
+
+    Default = AutoNewLine,
+  }
+
+}//namespace

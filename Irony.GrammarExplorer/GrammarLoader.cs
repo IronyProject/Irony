e9faf0bd494fc7628087248073f4b9bb7425291a
Roman Ivantsov <594969+rivantsov@users.noreply.github.com>
2011-08-01 01:14:39 -0700

    Do not take it - work in progress. New interpreter, first version.

--- Irony.GrammarExplorer/GrammarLoader.cs
+++ Irony.GrammarExplorer/GrammarLoader.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region License
+/* **********************************************************************************
+ * Copyright (c) Roman Ivantsov
+ * This source code is subject to terms and conditions of the MIT License
+ * for Irony. A copy of the license can be found in the License.txt file
+ * at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * MIT License.
+ * You must not remove this notice from this software.
+ * **********************************************************************************/
+// This file and all functionality of dynamic assembly reloading was contributed by Alexey Yakovlev (yallie)
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

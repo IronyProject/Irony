c539cf7cd8562d796cb1b92a10e40a65c6649086
Michael Tindal <babyplatypus@me.com>
2011-08-22 21:20:32 +0300

    Refactor HereDocTerminal class, hook into ParsingContext to help skip already consumed lines, and make OutputTerminal protected set.

--- Irony/Parsing/Terminals/HeredocTerminal.cs
+++ Irony/Parsing/Terminals/HeredocTerminal.cs
@@ -16,7 +16,7 @@ using System.Text;
 using System.Globalization;
 
 namespace Irony.Parsing {
-    
+
     [Flags]
     public enum HereDocOptions {
         None = 0,
@@ -41,18 +41,36 @@ namespace Irony.Parsing {
                 } catch { }
                 return 0;
             }
+
+            internal string GetTag(ParsingContext context, ISourceStream source) {
+                char[] endOfTokenMarker = context.Language.GrammarData.WhitespaceAndDelimiters.ToCharArray();
+                var endOfTagPos = source.Text.IndexOfAny(endOfTokenMarker, source.PreviewPosition + this.Start.Length);
+                //We could not find the end of the token
+                if (endOfTagPos == -1)
+                    return null;
+                source.PreviewPosition = endOfTagPos;
+                var tagLength = source.PreviewPosition - source.Location.Position - this.Start.Length;
+                return source.Text.Substring(source.PreviewPosition - tagLength, tagLength);
+            }
+
+            internal bool CheckEnd(ParsingContext context, ISourceStream source, string line, string tag) {
+                if (this.Flags.HasFlag(HereDocOptions.AllowIndentedEndToken)) {
+                    if (line.Trim().IndexOf(tag, context.Language.Grammar.CaseSensitive ? StringComparison.CurrentCulture : StringComparison.CurrentCultureIgnoreCase) == 0) {
+                        return true;
+                    }
+                } else {
+                    if (source.MatchSymbol(tag, !context.Language.Grammar.CaseSensitive)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
+
         class HereDocSubTypeList : List<HereDocSubType> {
             internal void Add(string start, HereDocOptions flags) {
                 base.Add(new HereDocSubType(start, flags));
             }
-        }
-        #endregion
-
-        #region HereDocConsumedLine
-        struct HereDocConsumedLine {
-            public int offset;
-            public string text;
         }
         #endregion
 
@@ -65,30 +83,73 @@ namespace Irony.Parsing {
         public HereDocTerminal(string name, string start, HereDocOptions options)
             : this(name) {
             _subtypes.Add(start, options);
-            outputTerminal = this.OutputTerminal;
         }
 
         public HereDocTerminal(string name, string start, HereDocOptions options, Terminal output)
             : this(name, start, options) {
-                outputTerminal = output;
+            this.OutputTerminal = output;
         }
 
-        public void AddStart(string startSymbol, HereDocOptions hereDocOptions) {
+        public void AddSubType(string startSymbol, HereDocOptions hereDocOptions) {
             _subtypes.Add(startSymbol, hereDocOptions);
         }
         #endregion
 
         #region Private fields
         private readonly HereDocSubTypeList _subtypes = new HereDocSubTypeList();
-
-        private char[] endOfLineMarker = { '\n', '\r' };
-
-        private char[] endOfTokenMarker = { ' ', '\n', '\r' };
-
-        private Terminal outputTerminal;
         #endregion
 
-        #region Overrides
+        #region Private methods
+        private HereDocSubType DetermineSubType(ParsingContext context, ISourceStream source) {
+            foreach (HereDocSubType s in _subtypes) {
+                if (source.MatchSymbol(s.Start, !context.Language.Grammar.CaseSensitive)) {
+                    return s;
+                }
+            }
+            return null;
+        }
+
+        private HashSet<int> CreateConsumedLines(ParsingContext context) {
+            object temp;
+            if (context.Values.TryGetValue("HereDocConsumedLines", out temp)) {
+                return temp as HashSet<int>;
+            } else {
+                HashSet<int> consumedLines = new HashSet<int>();
+                context.Values["HereDocConsumedLines"] = consumedLines;
+                return consumedLines;
+            }
+        }
+
+        private void SetUpEvent(ParsingContext context) {
+            char[] endOfLineMarker = context.Language.Grammar.LineTerminators.ToCharArray();
+            if (!context.Values.ContainsKey("HereDocEventCreated")) {
+                context.TokenCreated += (sender, args) => {
+                    if (args.Context.CurrentToken.Terminal == context.Language.Grammar.NewLine.OutputTerminal) {
+                        HashSet<int> consumedLines = CreateConsumedLines(context);
+                        ISourceStream csource = args.Context.Source;
+                        var eolPos = csource.Text.IndexOfAny(endOfLineMarker, csource.PreviewPosition);
+                        if (consumedLines.Contains(eolPos + 1)) {
+                            int highestPosition = eolPos + 1;
+                            foreach (int position in consumedLines) {
+                                if (position > highestPosition)
+                                    highestPosition = position;
+                            }
+                            var nextLinePosition = csource.Text.IndexOfAny(endOfLineMarker, highestPosition);
+                            csource.PreviewPosition = nextLinePosition;
+                        }
+                    }
+                };
+                context.Values["HereDocEventCreated"] = true;
+            }
+        }
+        #endregion
+
+        #region Overrides and methods
+        public override void Init(GrammarData grammarData) {
+            base.Init(grammarData);
+            _subtypes.Sort(HereDocSubType.LongerStartFirst);
+        }
+
         public override IList<string> GetFirsts() {
             List<string> firsts = new List<string>();
             foreach (var subtype in _subtypes) {
@@ -98,38 +159,24 @@ namespace Irony.Parsing {
         }
 
         public override Token TryMatch(ParsingContext context, ISourceStream source) {
-            List<HereDocConsumedLine> consumedLines;
-            if (context.Values.ContainsKey("HereDocConsumedLines")) {
-                consumedLines = context.Values["HereDocConsumedLines"] as List<HereDocConsumedLine>;
-            } else {
-                consumedLines = new List<HereDocConsumedLine>();
-                context.Values["HereDocConsumedLines"] = consumedLines;
-            }
+            HashSet<int> consumedLines = CreateConsumedLines(context);
+            SetUpEvent(context);
 
             // First determine which subtype we are working with:
-            _subtypes.Sort(HereDocSubType.LongerStartFirst);
-            HereDocSubType subtype = null;
-            foreach (HereDocSubType s in _subtypes) {
-                if (source.Text.IndexOf(s.Start, source.PreviewPosition) == source.PreviewPosition) {
-                    subtype = s;
-                    break;
-                }
-            }
+            HereDocSubType subtype = DetermineSubType(context, source);
+            char[] endOfLineMarker = context.Language.Grammar.LineTerminators.ToCharArray();
             if (subtype == null) {
                 return null;
             }
             //Find the end of the heredoc token
-            var newPos = source.Text.IndexOfAny(endOfTokenMarker, source.PreviewPosition + subtype.Start.Length);
-            //We could not find the end of the token
-            if (newPos == -1)
-                return source.CreateErrorToken(Resources.ErrNoEndForHeredoc);// "No end symbol for heredoc." 
-            source.PreviewPosition = newPos;
-            var tagLength = source.PreviewPosition - source.Location.Position - subtype.Start.Length;
-            var tag = source.Text.Substring(source.PreviewPosition - tagLength, tagLength);
-            newPos = source.Text.IndexOfAny(endOfLineMarker, source.PreviewPosition);
-            if (newPos == -1)
+            var tag = subtype.GetTag(context, source);
+            var endOfTagPos = source.PreviewPosition;
+            if (tag == null)
                 return source.CreateErrorToken(Resources.ErrNoEndForHeredoc);
-            source.PreviewPosition = newPos;
+            var endOfLinePos = source.Text.IndexOfAny(endOfLineMarker, source.PreviewPosition);
+            if (endOfLinePos == -1)
+                return source.CreateErrorToken(Resources.ErrNoEndForHeredoc);
+            source.PreviewPosition = endOfLinePos;
 
             source.PreviewPosition++;
             var value = new StringBuilder();
@@ -138,49 +185,26 @@ namespace Irony.Parsing {
                 var eolPos = source.Text.IndexOfAny(endOfLineMarker, source.PreviewPosition);
                 if (eolPos == -1)
                     break;
+                bool doExtraShift = (source.PreviewChar == '\r' && source.NextPreviewChar == '\n');
+                if (doExtraShift) eolPos += 1;
+                source.PreviewPosition = eolPos + 1;
                 var nextEol = source.Text.IndexOfAny(endOfLineMarker, eolPos + 1);
                 var line = nextEol == -1 ? source.Text.Substring(eolPos + 1) : source.Text.Substring(eolPos + 1, nextEol - eolPos - 1);
-                if (subtype.Flags.HasFlag(HereDocOptions.AllowIndentedEndToken)) {
-                    var endPos = source.Text.IndexOf(tag, eolPos + 1);
-                    if ((endPos != -1 && nextEol != -1 && endPos < nextEol) || (nextEol == -1 && endPos != -1)) {
-                        HereDocConsumedLine cline = new HereDocConsumedLine();
-                        cline.offset = eolPos + 1;
-                        cline.text = line;
-                        consumedLines.Add(cline);
-                        endFound = true;
-                    }
-                } else {
-                    if (line.Length >= tag.Length && line.Substring(0, tag.Length) == tag) {
-                        HereDocConsumedLine cline = new HereDocConsumedLine();
-                        cline.offset = eolPos + 1;
-                        cline.text = line;
-                        consumedLines.Add(cline);
-                        endFound = true;
-                    }
+
+                endFound = subtype.CheckEnd(context, source, line, tag);
+
+                bool foundLine = consumedLines.Contains(eolPos + 1);
+                if (!foundLine) {
+                    if (!endFound) value.AppendLine(line);
+                    consumedLines.Add(eolPos + 1);
                 }
-                if (!endFound) {
-                    bool foundLine = false;
-                    foreach (HereDocConsumedLine consumed in consumedLines) {
-                        if (consumed.text == line && consumed.offset == eolPos + 1) {
-                            foundLine = true;
-                            break;
-                        }
-                    }
-                    if (!foundLine) {
-                        value.AppendLine(line);
-                        HereDocConsumedLine cline = new HereDocConsumedLine();
-                        cline.offset = eolPos + 1;
-                        cline.text = line;
-                        consumedLines.Add(cline);
-                    }
-                    source.PreviewPosition = nextEol + 1;
-                }
+                source.PreviewPosition = nextEol + 1;
             }
             if (endFound) {
-                Token token = source.CreateToken(outputTerminal);
+                Token token = source.CreateToken(this.OutputTerminal);
                 token.Value = value.ToString();
-                if(source.Text.IndexOfAny(endOfLineMarker,newPos) != newPos)
-                    source.PreviewPosition = newPos;
+                if (source.Text.IndexOfAny(endOfLineMarker, endOfTagPos) != endOfTagPos)
+                    source.PreviewPosition = endOfTagPos;
                 return token;
             } else
                 return source.CreateErrorToken(Resources.ErrNoEndForHeredoc);

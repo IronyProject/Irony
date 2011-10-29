e1e54e6524830d43706a5082a4dbfb384e9c6284
Roman Ivantsov <594969+rivantsov@users.noreply.github.com>
2011-10-29 11:19:00 -0700

    Fixed FreeTextLiteral

---
+++ Irony.Interpreter/Ast/Functions/FunctionDefNode.cs
@@ -0,0 +1,77 @@
+﻿#region License
+/* **********************************************************************************
+ * Copyright (c) Roman Ivantsov
+ * This source code is subject to terms and conditions of the MIT License
+ * for Irony. A copy of the license can be found in the License.txt file
+ * at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * MIT License.
+ * You must not remove this notice from this software.
+ * **********************************************************************************/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Irony.Interpreter;
+using Irony.Parsing;
+
+namespace Irony.Interpreter.Ast {
+
+  //A node representing function definition
+  public class FunctionDefNode : AstNode {
+    public AstNode NameNode;
+    public AstNode Parameters;
+    public AstNode Body;
+    bool _languageCaseSensitive; 
+
+     
+    public override void Init(ParsingContext context, ParseTreeNode treeNode) {
+      base.Init(context, treeNode);
+      _languageCaseSensitive = context.Language.Grammar.CaseSensitive;
+      //child #0 is usually a keyword like "def"
+      NameNode = AddChild("Name", treeNode.MappedChildNodes[1]);
+      Parameters = AddChild("Parameters", treeNode.MappedChildNodes[2]);
+      Body = AddChild("Body", treeNode.MappedChildNodes[3]);
+      AsString = "<Function " + NameNode.AsString + ">";
+      Body.SetIsTail(); //this will be propagated to the last statement
+    }
+
+    public override void Reset() {
+      DependentScopeInfo = null; 
+      base.Reset();
+    }
+
+    protected override object DoEvaluate(ScriptThread thread) {
+      thread.CurrentNode = this;  //standard prolog
+      lock (LockObject) {
+        if (DependentScopeInfo == null)
+          base.DependentScopeInfo = new ScopeInfo(this, _languageCaseSensitive);
+        // In the first evaluation the parameter list will add parameter's SlotInfo objects to Scope.ScopeInfo
+        thread.PushScope(DependentScopeInfo, null);
+        Parameters.Evaluate(thread);
+        thread.PopScope();
+        //Set Evaluate method and invoke it later
+        this.Evaluate = EvaluateAfter;
+      }
+      var result = Evaluate(thread);
+      thread.CurrentNode = Parent; //standard epilog
+      return result;
+    }
+
+    private object EvaluateAfter(ScriptThread thread) {
+      thread.CurrentNode = this;  //standard prolog
+      var closure = new Closure(thread.CurrentScope, this);
+      if (NameNode != null)
+        NameNode.SetValue(thread, closure); 
+      thread.CurrentNode = Parent; //standard epilog
+      return closure;
+    }
+
+    public override void SetIsTail() {
+      //ignore this call, do not mark this node as tail, it is meaningless
+    }
+  }//class
+
+}//namespace
---
+++ Irony.Tests/IdentifierTerminalTests.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Irony.Parsing;
+
+namespace Irony.Tests {
+#if USE_NUNIT
+    using NUnit.Framework;
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+  [TestClass]
+  public class IdentifierTerminalTests : TerminalTestsBase {
+
+    [TestMethod]
+    public void TestCSharpIdentifier() {
+      SetTerminal(TerminalFactory.CreateCSharpIdentifier("Identifier"));
+      TryMatch("x ");
+      Assert.IsTrue(_token.Terminal.Name == "Identifier", "Failed to parse identifier");
+      Assert.IsTrue((string)_token.Value == "x", "Failed to parse identifier");
+      TryMatch("_a01 ");
+      Assert.IsTrue(_token.Terminal.Name == "Identifier", "Failed to parse identifier starting with _");
+      Assert.IsTrue((string)_token.Value == "_a01", "Failed to parse identifier starting with _");
+
+      TryMatch("0abc ");
+      Assert.IsTrue(_token == null, "Erroneously recognized an identifier.");
+
+      TryMatch(@"_\u0061bc ");
+      Assert.IsTrue(_token.Terminal.Name == "Identifier", "Failed to parse identifier starting with _");
+      Assert.IsTrue((string)_token.Value == "_abc", "Failed to parse identifier containing escape sequence \\u");
+
+      TryMatch(@"a\U00000062c_ ");
+      Assert.IsTrue(_token.Terminal.Name == "Identifier", "Failed to parse identifier starting with _");
+      Assert.IsTrue((string)_token.Value == "abc_", "Failed to parse identifier containing escape sequence \\U");
+    }//method
+
+    [TestMethod]
+    public void TestIdentifierCaseRestrictions() {
+      var id = new IdentifierTerminal("identifier"); 
+      id.CaseRestriction = CaseRestriction.None;
+      SetTerminal(id);
+
+      TryMatch("aAbB");
+      Assert.IsTrue(_token != null, "Failed to scan an identifier aAbB.");
+
+      id.CaseRestriction = CaseRestriction.FirstLower;
+      SetTerminal(id);
+      TryMatch("BCD");
+      Assert.IsTrue(_token == null, "Erroneously recognized an identifier BCD with FirstLower restriction.");
+      TryMatch("bCd ");
+      Assert.IsTrue(_token != null && _token.ValueString == "bCd", "Failed to scan identifier bCd with FirstLower restriction.");
+
+      id.CaseRestriction = CaseRestriction.FirstUpper;
+      SetTerminal(id);
+      TryMatch("cDE");
+      Assert.IsTrue(_token == null, "Erroneously recognized an identifier cDE with FirstUpper restriction.");
+      TryMatch("CdE");
+      Assert.IsTrue(_token != null && _token.ValueString == "CdE", "Failed to scan identifier CdE with FirstUpper restriction.");
+
+      id.CaseRestriction = CaseRestriction.AllLower;
+      SetTerminal(id);
+      TryMatch("DeF");
+      Assert.IsTrue(_token == null, "Erroneously recognized an identifier DeF with AllLower restriction.");
+      TryMatch("def");
+      Assert.IsTrue(_token != null && _token.ValueString == "def", "Failed to scan identifier def with AllLower restriction.");
+
+      id.CaseRestriction = CaseRestriction.AllUpper;
+      SetTerminal(id);
+      TryMatch("EFg ");
+      Assert.IsTrue(_token == null, "Erroneously recognized an identifier EFg with AllUpper restriction.");
+      TryMatch("EFG");
+      Assert.IsTrue(_token != null && _token.ValueString == "EFG", "Failed to scan identifier EFG with AllUpper restriction.");
+    }//method
+
+    /*
+    [TestMethod]
+    public void TestSqlIdentifier() {
+      var id = TerminalFactory.CreateSqlExtIdentifier(_grammar, "identifier");
+      SetTerminal(id.OutputTerminal);
+      TryMatch(@"[a b c]  ");
+      Assert.IsTrue((string)_token.Value == "a b c", "Failed to process bracketted identifier [a b c]");
+      TryMatch("\"a b c\"  "); //"a b c"
+      Assert.IsTrue((string)_token.Value == "a b c", "Failed to process double-quoted identifier \"a b c\"");
+    }
+    */
+
+  }//class
+}//namespace
+
+
+/* example for c# from 3.0 spec:
+class @class
+{
+	public static void @static(bool @bool) {
+		if (@bool)
+			System.Console.WriteLine("true");
+		else
+			System.Console.WriteLine("false");
+	}	
+}
+class Class1
+{
+	static void M() {
+		cl\u0061ss.st\u0061tic(true);
+	}
+}
+
+*/

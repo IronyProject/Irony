42433b24e8b527014a9ad253578a7375fac30a99
rivantsov <rivantsov@localhost>
2013-09-24 13:22:48 -0700

    fixing files/folders

--- Irony.Samples/Playground_Grammar.cs
+++ Irony.Samples/Playground_Grammar.cs
@@ -4,23 +4,33 @@ using System.Collections.Generic;
 using System.Text;
 using Irony.Parsing;
 using System.Globalization;
+using Irony.Interpreter;
+using Irony.Interpreter.Ast;
 
 namespace Irony.Samples.TestBed {
-  [Language("ServerGrammar", "1", "Http Server name Grammar")]
-  public class ServerGrammar : Irony.Parsing.Grammar {
-    public ServerGrammar()  : base() {
-      var serverNamevalue = new FreeTextLiteral("serverNameValue", FreeTextOptions.None, " ", ";");
-      var semicolon = ToTerm(";");
-      var httpCoreServerName = new NonTerminal("httpCoreServerName");
-      var httpCoreServerNameItems = new NonTerminal("httpCoreServerNameItems");
+  public class HTMLGrammar : Irony.Parsing.Grammar {
+    public HTMLGrammar() {
+      KeyTerm leftAnguralBracket = new KeyTerm("<", "LeftAngularBarakcet");
+      KeyTerm rightAnguralBracket = new KeyTerm(">", "RightAngularBarakcet");
+      KeyTerm leftAngularBracketEndTag = new KeyTerm("</", "LeftAngularBracketEndTag");
+      KeyTerm rightAngularBracketEndTag = new KeyTerm("/>", "RightAngularBracketEndTag");
 
-      httpCoreServerName.Rule = "server_name" + httpCoreServerNameItems + semicolon;
-      httpCoreServerNameItems.Rule = MakePlusRule(httpCoreServerNameItems, serverNamevalue);
 
-      this.Root = httpCoreServerName;
+      NonTerminal element = new NonTerminal("Element");
+      NonTerminal emptyElementTag = new NonTerminal("EmptyElementTag");
+      NonTerminal startTag = new NonTerminal("StartTag");
+      NonTerminal content = new NonTerminal("Content");
+      NonTerminal endTag = new NonTerminal("EndTag");
+      RegexBasedTerminal name = new RegexBasedTerminal("Name", "\\w+");
 
+      element.Rule = emptyElementTag | startTag + content + endTag;
+      emptyElementTag.Rule = leftAnguralBracket + name + rightAngularBracketEndTag;
+      startTag.Rule = leftAnguralBracket + name + rightAnguralBracket;
+      endTag.Rule = leftAngularBracketEndTag + name + rightAnguralBracket;
+      content.Rule = MakeStarRule(content, element);
+
+      this.Root = element;
     }
-
   }
 }
 

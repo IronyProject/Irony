cc991349ece0795e88fcd6d0016729c9a493777c
Dávid Németi <david.nemeti@gmail.com>
2023-01-26 15:25:48 +0100

    ParseTreeSimplicityTests.TestIfElse: testing simplicity of parse tree

---
+++ Irony.Tests/ParseTreeSimplicityTests.cs
@@ -0,0 +1,59 @@
+using System;
+using Irony.Parsing;
+
+namespace Irony.Tests {
+#if USE_NUNIT
+    using NUnit.Framework;
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+#else
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+  [TestClass]
+  public class ParseTreeSimplicityTests {
+
+    #region Grammars
+    public class IfElseGrammar : Grammar {
+      public IfElseGrammar()  {
+        var id = new IdentifierTerminal("id");
+
+        Root = new NonTerminal("root");
+        Root.Rule = ToTerm("if") + id + id + (ToTerm("else") + id).Q();
+      }
+    }//if else grammar class
+
+    #endregion
+
+    [TestMethod]
+    public void TestIfElse() {
+
+      var grammar = new IfElseGrammar();
+      var parser = new Parser(grammar);
+      TestHelper.CheckGrammarErrors(parser);
+
+      var parseTree = parser.Parse("if cond a");
+      TestHelper.CheckParseErrors(parseTree);
+      Assert.IsNotNull(parseTree.Root, "Root not found.");
+      Assert.AreEqual("root", parseTree.Root.Term.Name);
+      Assert.AreEqual("if", parseTree.Root.ChildNodes[0].Term.Name);
+      Assert.AreEqual("id", parseTree.Root.ChildNodes[1].Term.Name);
+      Assert.AreEqual("id", parseTree.Root.ChildNodes[2].Term.Name);
+
+      parseTree = parser.Parse("if cond a else b");
+      var errorMessage = $"Actual parse tree:{Environment.NewLine}{parseTree.ToXml()}";
+
+      TestHelper.CheckParseErrors(parseTree);
+      Assert.IsNotNull(parseTree.Root, "Root not found.");
+      Assert.AreEqual("root", parseTree.Root.Term.Name, errorMessage);
+      Assert.AreEqual("if", parseTree.Root.ChildNodes[0].Term.Name, errorMessage);
+      Assert.AreEqual("id", parseTree.Root.ChildNodes[1].Term.Name, errorMessage);
+      Assert.AreEqual("id", parseTree.Root.ChildNodes[2].Term.Name, errorMessage);
+      Assert.AreEqual("else", parseTree.Root.ChildNodes[3].ChildNodes[0].Term.Name, errorMessage);
+      Assert.AreEqual("id", parseTree.Root.ChildNodes[3].ChildNodes[1].Term.Name, errorMessage);
+
+    }
+
+  }//class
+}//namespace
